d3821241850f761c592d34c6503b33e904f4d577
Jenny Kathke <142265026+JenBytes@users.noreply.github.com>
2025-08-26 01:33:28 +0200

    minimal customizations

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'JenByte';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+// Social Networks
+export const INSTAGRAM_URL = "https://instagram.com/jen.byte"
+export const GITHUB_URL = "https://github.com/jenbytes"
+export const SPOTIFY_URL = "https://spotify.jenbyte.com"
